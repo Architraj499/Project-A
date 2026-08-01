930d76a2c2c8cf6002bb9aa1b5baf01be1bdcba5
Archit Raj <architraj499@gmail.com>
2026-08-01 19:26:08 +0530

    Enhance SEO and Accessibility Across Multiple Pages
    - Updated meta tags for SEO optimization in BoardsEps.html, BoardsHd.html, Boardseco.html, and cuet/Accountancy.html to improve search engine visibility.
    - Improved accessibility by adding loading attributes to images and iframes across various HTML files.
    - Standardized titles and descriptions for consistency and clarity in user experience.
    - Modified subject headings and meta information to reflect accurate course details.
    - Corrected links and references in sitemap.xml to ensure proper navigation.
    - Enhanced structured data for better indexing by search engines.

--- JavaScript/config/version.js
+++ JavaScript/config/version.js
@@ -1,7 +1,7 @@
 export const APP = {
     NAME: "Asprients",
-    VERSION: "2.1.2",
-    RELEASE: "Stable",
+    VERSION: "2.1.3",
+    RELEASE: "Beta",
      TERMS_VERSION:"1.1",
     MOTTO: "Your Personal Study System"
 };
